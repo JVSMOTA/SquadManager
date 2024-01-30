b5107dbec28f6daba0c5896afff635d7d0bc66a5
JVSMOTA <jvmota.sb@gmail.com>
2024-01-30 14:51:24 -0300

    :sparkles: feat: initial commit

---
+++ src/test/java/br/com/SquadManager/SquadManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.SquadManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
