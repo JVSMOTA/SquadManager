1ff307b467139d7a24de29c5d7a6225c8bc6a6c4
JVSMOTA <jvmota.sb@gmail.com>
2024-01-31 08:44:39 -0300

    :sparkles: feat: aplicando constraint no nome da empresa

--- src/main/java/br/com/SquadManager/models/EmpresaModel.java
+++ src/main/java/br/com/SquadManager/models/EmpresaModel.java
@@ -1,13 +1,22 @@
 package br.com.SquadManager.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.Data;
+import org.apache.logging.log4j.message.Message;
+import org.aspectj.lang.annotation.DeclareError;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import java.util.UUID;
 
 @Data
 @Entity
-@Table(name = "TB_EMPRESAS")
+@Table(name = "TB_EMPRESAS",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_nome", columnNames = "nome")
+        }
+)
+
 public class EmpresaModel {
 
     @Id
