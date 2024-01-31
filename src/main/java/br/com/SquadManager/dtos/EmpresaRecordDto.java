80aabf0a229a3829bb63131f0f9311134ecd9355
JVSMOTA <jvmota.sb@gmail.com>
2024-01-31 15:51:27 -0300

    :bug: fix: removendo entidade desnecessária :(

--- src/main/java/br/com/SquadManager/dtos/EmpresaRecordDto.java
+++
@@ -1,6 +0,0 @@
-package br.com.SquadManager.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record EmpresaRecordDto(@NotBlank String nome) {
-}
--- src/main/java/br/com/SquadManager/exceptions/EmpresaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.SquadManager.exceptions;
-
-public class EmpresaNotFoundException extends SquadManagerException {
-    public EmpresaNotFoundException() {
-        super("Empresa not found.");
-    }
-}
